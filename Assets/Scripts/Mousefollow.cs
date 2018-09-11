21df4f2fa37f1f0926badc3f03701cc7982dd296
Benjamin Armour <barmour@mail.pima.edu>
2018-09-11 14:29:43 -0700

    Added 2 scripts and made some scenes
    Scripts were Mouse follow and hopefully the movement for character, the scenes are the asset playground for creating and testing assets, Level one for the first level, Menu for the start screen and Death screen for the lose state.

---
+++ Assets/Scripts/Mousefollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousefollow : MonoBehaviour {
+    public float speedH = 2f;
+    public float speedV = 2f;
+
+    private float Dis = 0f;
+    private float Dat = 0f;
+ 
+	// Update is called once per frame
+	void Update () {
+        //the mouse inputs are used in the EulerAngles of the transform.
+        //i used clamp to stop it from looking up to much or down to much.
+        Dis -= speedH * Input.GetAxis("Mouse Y");
+        Dat += speedV * Input.GetAxis("Mouse X");
+        print(Dis);
+       
+        
+            transform.eulerAngles = new Vector3(Mathf.Clamp(Dis, -89, 89), Dat, 0f);
+        
+       
+    }
+}
---
+++ Assets/Scripts/NewMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour {
+
+    public Transform Character;
+    public Rigidbody PhysicBody;
+    public Transform Camera;
+    private Vector3 movement;
+    public float Thrust;
+    public float Hop;
+
+  
+    Vector3 JumpSize = new Vector3(0f, 1f, 0f);
+
+    
+    void Update()
+    {
+        movement.z = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+
+        if( Input.GetButtonDown("Jump") ) {
+            PhysicBody.AddRelativeForce(JumpSize * Hop, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 worldMovement = Camera.TransformDirection( movement ) * Thrust;
+        worldMovement.y = PhysicBody.velocity.y;
+
+        PhysicBody.velocity = worldMovement;
+    }
+}
