17dafe5471f188cf655c27ca53aa28a5d0889bde
Benjamin Armour <barmour@mail.pima.edu>
2018-09-23 18:22:05 -0700

    buttonstuff

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string loadlevel;
+    public void loading()
+    {
+        SceneManager.LoadScene(loadlevel);
+    }
 }
