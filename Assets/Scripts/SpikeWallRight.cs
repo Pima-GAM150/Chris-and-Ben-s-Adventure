982c30489e41f018659fc98d870083a38dc11bfe
Benjamin Armour <barmour@mail.pima.edu>
2018-09-27 17:02:23 -0700

    Added traps, Worked with Chris to add lights, finished most of the game.
    Made ton of scripts for the traps, made a light prefab with particle effect using realtime, added statue of satyr for fun at end of level, music is in the game now and some sound effects as well.

---
+++ Assets/Scripts/SpikeWallRight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeWallRight : MonoBehaviour {
+
+	public float offset;
+	public float Secondoffset;
+	public float reset;
+	public float amountdirection = -.05f;
+	public float pause;
+	private float timer;
+    private float seconds = 1f;
+
+
+	void Start () {
+		timer = seconds += Time.deltaTime;
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer = seconds += Time.deltaTime;
+		if (timer <= offset)
+        {
+            transform.position = new Vector3(transform.position.x + amountdirection, transform.position.y, transform.position.z);
+        }
+        if (timer >= Secondoffset)
+        {
+          transform.position = new Vector3(transform.position.x - amountdirection, transform.position.y, transform.position.z);
+            
+        }
+        
+        if (timer >= reset)
+        {
+            seconds = 0;
+        }
+		
+	}
+}
